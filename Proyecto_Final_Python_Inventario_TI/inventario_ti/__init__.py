b9e48aaf303fb315ed72708d9bb6938221be524a
Formacion <Formacion@MacBook-Pro-de-Pedro.local>
2026-07-09 10:16:07 +0200

    Actualización

--- Proyecto_Final_Python_Inventario_TI/inventario_ti/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Paquete de apoyo para el proyecto final de Programación Python."""
-
-__version__ = "1.0.0"
--- Tema09/src/soporte_ti/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Paquete soporte_ti.
-
-Agrupa módulos sencillos para operaciones típicas de soporte:
-validación de red y generación de reportes.
-"""
-
-__version__ = "1.0.0"
